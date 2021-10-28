a5ba662cc3f6b7b56c47a00f9a10911523baab5d
Angel <angel333gomez@gmail.com>
2021-10-28 13:46:38 -0300

    Deleted ChakraUI

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,7 @@
 import '../styles/globals.css'
 
-import { ChakraProvider } from "@chakra-ui/react"
-
 function MyApp({ Component, pageProps }) {
-  return
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
