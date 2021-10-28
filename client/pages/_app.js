8df3c6328f6d7655f8ce625ecb74ef2da8b060da
Angel <angel333gomez@gmail.com>
2021-10-28 19:07:04 -0300

    Building Landing Page 1

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,8 @@
 import { createGlobalStyle} from 'styled-components';
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient("https://xyzcompany.supabase.co", "public-anon-key")
 
 const GlobalStyle = createGlobalStyle`
 html {
