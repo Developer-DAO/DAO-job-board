f513bebb2149454ff4e0e8a771bfb4527182abcd
Angel <angel333gomez@gmail.com>
2021-10-28 14:43:26 -0300

    Added Styled-Components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html {
+  background: #F7FAFC;
+  display:block;
+  height: 100%;
+  max-width: 100%;
+  margin:0 auto;
+  padding: 0;
+}
+
+body {
+  background-color:#F7FAFC;
+  min-height: 100%;
+  padding: 1rem;
+  margin-top:0;
+  font-family: InterVariable, -apple-system, BlinkMacSystemFont, system-ui, sans-serif;
+}
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
