98788c761298d06f7d0345c7b5b9bd2e091fa896
Angel <angel333gomez@gmail.com>
2021-10-28 18:13:12 -0300

    Added About / Joblist Pages

---
+++ client/pages/jobs/JobFilter.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+function JobFilter() {
+  return (
+    <>
+      <p>YES</p>
+    </>
+  )
+};
+
+export default JobFilter;
+
+const JobFilterBox = styled.div`
+
+`;
---
+++ client/pages/navigation/Navbar.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+function Navbar() {
+  return (
+
+  )
+}
+
+export default Navbar;
