f5b5fcb84880515c6e28edeed5cb8bb501babd65
Angel <angel333gomez@gmail.com>
2021-10-28 21:10:18 -0300

    Building Landing Page

---
+++ client/pages/landing/Landing.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export default function Landing() {
+  return (
+      <>
+
+      </>
+  )
+}
+
+const Hero = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`
+const Copy = styled.div`
+  position: relative;
+  margin-left: 10rem;
+  max-width: 60%;
+
+  @media (max-width: 650px) {
+    margin: auto;
+    top: 5rem;
+  }
+`
+const Title = styled.h1`
+`;
+
+const CopyBottom = styled.h2`
+color: gray;
+`;
+
+const List = styled.li`
+  margin: 10px;
+  margin-left: -25px;
+`;
+
+const ButtonSection = styled.div`
+  margin-left: 10rem;
+  @media (max-width: 650px) {
+    margin-left: 25%;
+    margin-top: 7rem;
+  }
+`;
+
+const FindDevsButton = styled.button`
+
+`;
+
+const FindJobsButton = styled.button`
+
+`;
--- client/pages/navigation/Navbar.js
+++ client/pages/navigation/Navbar.js
@@ -1,24 +1,44 @@
 import styled from 'styled-components';
+import icon from '../../public/DevDAO.svg';
 
 export default function Navbar() {
   return (
     <NavBox>
       <NavbarTitle>
-        DevDAO Job Board
+        <svg src={icon}/>DevDAO Job Board
       </NavbarTitle>
+      <MainMenu>
+        <Navs
+          href='/jobs'
+        >
+          Job List
+        </Navs>
+
+        <Navs
+          href='/developers'
+        >
+          Developers
+        </Navs>
+
+        <Navs
+          href='/post-job'
+        >
+          Post a Job
+        </Navs>
+      </MainMenu>
     </NavBox>
   )
 };
 
-const NavBox = styled.div`
-  display: block;
+const NavBox = styled.nav`
+  display: flex;
   background-color: #F7FAFC;
   justify-content: space-between;
   text-align: center;
   z-index: 1;
   width: 100%;
   height: 10%;
-  border-bottom: solid 1px gray;
+  border-bottom: solid 2px #E2E8F0;
   opacity: 0.8;
   overflow: hidden;
 `;
@@ -27,12 +47,11 @@ const NavbarTitle = styled.h1`
   display: block;
   float: left;
   font-size: 0.875rem;
-  font-weight: bold;
   color: #4A5568;
 `;
 
 const Navs = styled.a`
-  color: white;
+  color: black;
   padding: 0.45rem;
   margin: 0 0.25rem;
   cursor: pointer;
@@ -51,14 +70,12 @@ const Navs = styled.a`
   }
 `;
 
-const GeneralLinks = styled.div`
-  display: inline-block;
+const MainMenu = styled.div`
   align-items: center;
   text-align: center;
-  float: center;
 `;
 
-const AuthLinks = styled.div`
+const Hamburgermenu = styled.div`
   display: inline-block;
   align-items: right;
   text-align: right;
