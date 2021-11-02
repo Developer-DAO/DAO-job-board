e60af7715b9f8c41ad003881e423509cb5a5d30c
Angel <angel333gomez@gmail.com>
2021-11-02 00:01:48 -0300

    Added New Company List Styling

--- client/pages/companies/CompanyItem.js
+++ client/pages/companies/CompanyItem.js
@@ -5,14 +5,32 @@ NameSection,
 ProfileImageCover,
 ProfileImage} from '../../styles/styles';
 
+import Button from '../../styles/UI-Components/Button';
+
+import styled from 'styled-components';
+
 export default function CompanyItem() {
   return (
     <ItemBox>
-
-      <NameSection>
-        Company
-      </NameSection>
-
+      <LinkCover href='/'>
+        <NameSection>
+          Company Name
+        </NameSection>
+      </LinkCover>
+      <ProfileImageCover>
+        <ProfileImage src='/ETHEREUM.png'/>
+      </ProfileImageCover>
+      <CompanyJobCount>
+        Open Jobs: 5
+      </CompanyJobCount>
+      <Button styling='normal'>
+        View More
+      </Button>
     </ItemBox>
   )
 }
+
+const CompanyJobCount = styled.em`
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
--- client/pages/companies/CompanyList.js
+++ client/pages/companies/CompanyList.js
@@ -1,11 +1,17 @@
-import { Box, Break, GridList} from "../../styles/styles";
+import { ListWrap, ListTop, Break, GridList} from "../../styles/styles";
 import CompanyItem from "./CompanyItem";
+import styled from 'styled-components';
 
 export default function CompanyList() {
   return (
+    <ListWrap>
+      <ListTop>
+        Partners
+      </ListTop>
       <GridList>
         <CompanyItem/>
         <CompanyItem/>
       </GridList>
+    </ListWrap>
   );
 }
