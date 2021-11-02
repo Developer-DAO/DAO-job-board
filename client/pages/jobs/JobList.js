8f6276ee2658684be7ec2bdbda142d68d72a7b25
Angel <angel333gomez@gmail.com>
2021-11-01 23:14:37 -0300

    Fixing Build 4

---
+++ client/pages/jobs/JobList.js
@@ -0,0 +1,11 @@
+import {Box, Title} from '../../styles/styles';
+
+function JobList() {
+  return (
+      <Box>
+          <Title>TO BE BUILT</Title>
+      </Box>
+  )
+}
+
+export default JobList;
