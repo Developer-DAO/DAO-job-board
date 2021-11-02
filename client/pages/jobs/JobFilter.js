e05d5ca6175eda5e5ec6542317a51fa608d9cfca
Angel <angel333gomez@gmail.com>
2021-11-01 23:18:57 -0300

    Fixing Build 5

--- client/pages/jobs/JobFilter.js
+++ client/pages/jobs/JobFilter.js
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 
+import {Box, Title} from '../../styles/styles';
+
 function JobFilter() {
   return (
-    <>
-    </>
+    <Box>
+      <Title>TO BE BUILT</Title>
+    </Box>
   )
 };
 
 export default JobFilter;
 
-const JobFilterBox = styled.div`
+const JobFilterBox = styled(Box)`
 
 `;
