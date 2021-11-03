542a182677771fc40e6fe0a3d2320e081e0b3640
Angel <angel333gomez@gmail.com>
2021-11-03 00:42:42 -0300

    Fixing Job Item Styles

--- client/pages/Landing.js
+++ client/pages/Landing.js
@@ -5,7 +5,7 @@ import {
   List,
   ButtonSection,
   LinkWrap,
-  DevDAOIcon
+  IconWrap
 } from "../styles/styles";
 
 import Button from "../styles/UI-Components/Button";
@@ -14,9 +14,9 @@ import Image from "next/image";
 export default function Landing() {
   return (
     <Hero>
-      <DevDAOIcon style={{width: '10rem', height: '10rem'}}>
+      <IconWrap style={{width: '10rem', height: '10rem'}}>
         <Image width="200" height="200" src="/DevDAO.png" />
-      </DevDAOIcon>
+      </IconWrap>
       <Copy>
         <Title>Looking for Web3 talent?</Title>
         <LinkWrap href="/developers">
