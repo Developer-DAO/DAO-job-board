f5fa733ff34f5d132de1f7e9932a49cee7a4bf38
carlomigueldy <carlomigueldy@gmail.com>
2021-11-05 06:55:22 +0800

    Add supabase client

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_SECRET: process.env.SUPABASE_SECRET,
+    SUPABASE_PUBLIC_KEY: process.env.SUPABASE_PUBLIC_KEY,
+  }
 }
