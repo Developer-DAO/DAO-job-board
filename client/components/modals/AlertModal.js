1f7d9d4b7f3a3b6e8bde8023a528cfa45cb2d01a
Angel <angel333gomez@gmail.com>
2021-11-05 18:03:40 -0300

    New Deployment

--- client/components/modals/AlertModal.js
+++ client/components/modals/AlertModal.js
@@ -1,26 +1,13 @@
-export default function AlertModal() {
-  return (
-    <div>This is an alert</div>
-  )
-}
 
-import React, {Fragment} from 'react'
+export default function AlertModal({ removeAlert, alerts}) {
 
-//Redux
-import {connect} from 'react-redux'
-// import PropTypes from 'prop-types'
-import {removeAlert} from '../../redux/actions/alert'
-
-import './Alert.css'
-
-const Alerts = ({ removeAlert, alerts }) => {
   return (
     <div>
      {alerts !== null &&
      alerts.length > 0 &&
      alerts.map(alert =>(
 
-      <Fragment key={alert.id}>
+      <div key={alert.id}>
         <div className='backdrop' onClick={removeAlert}></div>
 
         <div className='alert'>
@@ -30,16 +17,8 @@ const Alerts = ({ removeAlert, alerts }) => {
          </div>
         </div>
 
-      </Fragment>
+      </div>
     ))}
   </div>
   )
 };
-
-
-
-const mapStateToProps = state => ({
-  alerts: state.alert
-})
-
-export default connect(mapStateToProps, {removeAlert})(Alerts)
