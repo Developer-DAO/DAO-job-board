8990b6014fac8bb5faf0af01573cd508232579af
Angel <angel333gomez@gmail.com>
2021-11-06 02:20:51 -0300

    Fixing Typescript 11

--- client/styles/ui-components/Button.tsx
+++ client/styles/ui-components/Button.tsx
@@ -3,8 +3,8 @@ import styled, { CSSProperties } from "styled-components";
 export type ButtonProps = {
   children?: React.ReactNode;
   styling: "normal" | "category" | "black" | "red" | "blue" | "positive" | "negative" | string;
-  type: any;
-  onClick: any;
+  type?: "reset" | "submit" | "button"| string;
+  onClick?: (event: React.MouseEvent);
 };
 
 export type ButtonStyle = {
