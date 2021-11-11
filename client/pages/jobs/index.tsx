5316d8e672b9f029bb8d7ea0de9c3eed33b843a9
Angel <angel333gomez@gmail.com>
2021-11-11 02:02:33 -0300

    Migrated Jobs Page to Chakra UI 3

--- client/pages/jobs/index.tsx
+++ client/pages/jobs/index.tsx
@@ -1,19 +1,22 @@
 import JobFilter from "../../components/jobs/JobFilter";
-import JobList from "../../components/jobs/JobList";
+import JobItem from "../../components/jobs/JobItem";
 
 import {Heading, Container, Text} from "@chakra-ui/react"
 
 export default function Index() {
   return (
     <Container
-    maxW="auto"
+    maxW={{lg: "70%", md: "90%", sm: "100%"}}
+    m="auto"
     textAlign="center">
       <Heading>Jobs</Heading>
       <Text as="i">
         Curated Web3 and blockchain jobs at companies or startups.
       </Text>
       <JobFilter />
-      <JobList />
+      <Container w="100%" maxW="100%">
+        <JobItem/>
+      </Container>
     </Container>
   );
 }
