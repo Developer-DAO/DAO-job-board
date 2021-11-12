a238f9d1e21305db119467f6313b0fc344afc129
Angel <angel333gomez@gmail.com>
2021-11-12 02:00:58 -0300

    Create-Profile Page 2

--- client/pages/create-profile/index.tsx
+++ client/pages/create-profile/index.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 
-import {Box, Heading, Container, Flex, SimpleGrid, Tag, TagLabel, Switch, Input, Textarea, Image, Select, } from "@chakra-ui/react";
+import AddExperience from '../../components/create-profile/add-experience.tsx';
+import AddEducation from '../../components/create-profile/add-education.tsx';
 
-import {ButtonBlack, ButtonGreen, ButtonRed} from "@chakra-ui/react";
+import {Box, Heading, Container, Flex, SimpleGrid, Tag, TagLabel, Stack, Switch, InputGroup, Input, InputLeftAddon, Text, Textarea, Image, Select, AccordionIcon,
+AccordionButton, AccordionPanel, AccordionItem,
+Accordion } from "@chakra-ui/react";
+
+import {ButtonBlack, ButtonGreen, ButtonRed} from "../../styles/ui-components/Chakra-Button";
 
 export default function CreateProfile() {
 
@@ -42,76 +47,136 @@ export default function CreateProfile() {
       mt="2.5%"
     >
 
-    <Flex
-    bg="none"
-    direction="column"
-    width="100%"
-    margin="auto"
-    textAlign="center"
-    boxSizing="border-box"
-    boxShadow="none"
-    flex={1}
-    >
-        <Heading size="sm">PFP</Heading>
-        <Image
-        w="8rem"
-        h="8rem"
-        margin="auto"
-        borderRadius="180px"
-        src="/DevDAO.png"
-        alt="developer"
-        >
-        </Image>
-    </Flex>
-
-    <Flex
+      <Flex
       bg="none"
       direction="column"
       width="100%"
       margin="auto"
+      textAlign="center"
       boxSizing="border-box"
       boxShadow="none"
-      flex={2}
-    >
-      <Container
-        p="0px"
-        mt="5px"
-        maxW={{sm: "100%"}}
-        display="inline-box"
+      flex={1}
       >
-        <Heading size="sm">Basic Details</Heading>
-        <Input/>
+          <Heading size="md">PFP</Heading>
+          <Image
+          w="8rem"
+          h="8rem"
+          margin="auto"
+          mt="5%"
+          borderRadius="180px"
+          src="/DevDAO.png"
+          alt="developer"
+          />
+          <Heading
+          mt="5%"
+          size="md">Content Visibility</Heading>
+          <Text  mt="2.5%" fontSize="xs">Choose what you want people to see on your profile</Text>
+
+          <Stack
+          width="50%"
+          m="auto"
+          mt="5%"
+          textAlign="left"
+          direction="column">
+
+            <Switch>Basic Details</Switch>
+            <Switch>PFP</Switch>
+            <Switch>Description</Switch>
+            <Switch>Username</Switch>
+            <Switch>Keywords</Switch>
+            <Switch>Experience</Switch>
+            <Switch>Education</Switch>
+          </Stack>
+      </Flex>
+
+      <Flex
+        bg="none"
+        direction="column"
+        width="100%"
+        margin="auto"
+        boxSizing="border-box"
+        boxShadow="none"
+        flex={2}
+      >
+        <Container
+          p="0px"
+          mt="5px"
+          maxW={{sm: "100%"}}
+          display="inline-box"
+        >
+          <Heading size="md">Basic Details</Heading>
+
+          <Text  mt="2.5%">Name</Text>
+          <Input
+          placeholder="Here goes your name"
+          />
+
+          <Text  mt="2.5%">Description</Text>
+          <Textarea
+          placeholder="Here goes a brief description of yourself"/>
+
+          <Heading  mt="2.5%" size="md">Identity</Heading>
+
+          <Text mt="2.5%">Username</Text>
+          <Text fontSize="xs">Make it easy for people to know it's you</Text>
+          <InputGroup>
+            <InputLeftAddon children="@"/>
+            <Input
+              placeholder="Write your favorite username"
+            />
+          </InputGroup>
+
+        <Text mt="2.5%">Profile Keywords</Text>
+        <Text fontSize="xs">Keywords help categorize your profile in skills and sectors</Text>
+        <ButtonBlack>
+          Choose Keywords
+        </ButtonBlack>
+
+        <SimpleGrid
+        mt="15px"
+        templateColumns="repeat(5, 2fr)"
+        autoRows="fit-content"
+        gap="0.5rem"
+        spacing={1}>
+          <Tag
+          w="fit-content"
+          size="md"
+          borderRadius="8px"
+          bgColor="#E2E9F0"
+          color="black"
+          ><TagLabel m="auto">First Keyword</TagLabel></Tag>
+        </SimpleGrid>
+
+          <Accordion mt="2.5%" allowToggle>
+            <AccordionItem>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">
+                    <Heading size="md">Experience</Heading>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              <AccordionPanel pb={4}>
+                <AddExperience/>
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">
+                    <Heading  size="md">Education</Heading>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              <AccordionPanel pb={4}>
+                <AddEducation/>
+              </AccordionPanel>
+            </AccordionItem>
+
+        </Accordion>
 
       </Container>
 
-      <Container
-        p="0px"
-        m="0"
-        maxW={{lg: "auto", md:"auto", sm: "100%"}}
-        display="inline-box"
-      >
-        <Textarea
-        placeholder="Here goes a brief description of the user"/>
-      </Container>
-
-    <SimpleGrid
-    mt="15px"
-    templateColumns="repeat(5, 2fr)"
-    autoRows="fit-content"
-    gap="0.5rem"
-    spacing={1}>
-      <ButtonBlack>
-        Choose Keywords
-      </ButtonBlack>
-      <Tag
-      size="md"
-      borderRadius="8px"
-      bgColor="#E2E9F0"
-      color="black"
-      ><TagLabel m="auto">First Keyword</TagLabel></Tag>
-    </SimpleGrid>
-
-    </Flex>
+      </Flex>
     </Box>
   )
 }
