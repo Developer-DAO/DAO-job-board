fc3bb3f9ee0856f6b0ebb38edfbbb42ef75c4c9f
Giancarlo Palavicini <kasthor@gmail.com>
2021-11-14 18:31:00 -0600

    feat(login): login with metamask button #4

--- client/pages/auth/index.tsx
+++ client/pages/auth/index.tsx
@@ -1,24 +1,51 @@
-import { Box, Heading } from "@chakra-ui/react";
-import {ButtonGreen} from "../../styles/ui-components/Chakra-Button";
+import {  Container, Stack, Heading, Flex, Center } from "@chakra-ui/react";
+import {
+  ButtonGreen,
+  ButtonGray,
+} from "../../styles/ui-components/Chakra-Button";
 import Image from "next/image";
+import { useEthers, useEtherBalance } from "@usedapp/core";
 
 export default function Index() {
   return <SignUp />;
 }
 
+// TODO: move to utils library
+function formatAccount(account: String, length = 10) {
+  const halve = Math.floor(length - 2) / 2;
+  return `${account.slice(0, halve + 2)}...${account.slice(-halve)}`;
+}
+
+// TODO: Move to components
+function ConnectButton() {
+  const { activateBrowserWallet, account } = useEthers();
+
+  const isConnected = () => !!account;
+  const handleOnConnectToWallet = () => {
+    activateBrowserWallet();
+  };
+  const Button = isConnected() ? ButtonGreen : ButtonGray;
+
+  return (
+    <Button onClick={handleOnConnectToWallet}>
+      <Flex gridGap={2}>
+        <Image width={15} height={15} src="/metamask.png" alt="icon" />
+        {account ? `Connected: ${formatAccount(account)}` : `Login with Wallet`}
+      </Flex>
+    </Button>
+  );
+}
+
 function SignUp() {
   return (
-    <Box>
-      <Heading>LOG-IN</Heading>
+    <Container>
+      <Stack>
+        <Heading>LOG-IN</Heading>
 
-      <div style={{ textAlign: "center" }}>
-        <ButtonGreen>
-          <span style={{ position: "relative", top: "2px" }}>
-            <Image width={15} height={15} src="/metamask.png" alt="icon"/>
-          </span>{" "}
-          Login with Wallet
-        </ButtonGreen>
-      </div>
-    </Box>
+        <Center>
+          <ConnectButton />
+        </Center>
+      </Stack>
+    </Container>
   );
 }
