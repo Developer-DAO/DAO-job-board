cd1f8b6c07d642c814526e3752682f43de0ae9e7
Giancarlo Palavicini <kasthor@gmail.com>
2021-11-15 22:03:38 -0600

    feat(login): login with metamask button #4 (#36)

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
 import Navbar from "../components/navigation/Navbar";
-import { ChakraProvider, ColorModeScript  } from "@chakra-ui/react"
-import {theme} from "../styles/theme";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+import { DAppProvider } from "@usedapp/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <Navbar />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <DAppProvider config={{}}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Navbar />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </DAppProvider>
   );
 }
 
