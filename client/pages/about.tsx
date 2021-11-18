593af9f383f7ee34b3c9f37ec4de8f4a2fd91a76
Jovi De Croock <decroockjovi@gmail.com>
2021-11-18 18:06:10 +0100

    test husky

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading } from "@chakra-ui/react";
 
 function About() {
   return (
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,5 @@
 import Landing from '../components/Landing';
 import Head from 'next/head';
-import Image from 'next/image';
 
 export default function Home() {
   return (
