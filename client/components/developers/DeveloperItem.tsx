5c65b18b16de428d5d4db3d2c4f694436716bf1c
Angel <angel333gomez@gmail.com>
2021-11-23 15:49:47 -0300

    Fixed Front-End Issues

--- client/components/developers/DeveloperItem.tsx
+++ client/components/developers/DeveloperItem.tsx
@@ -1,13 +1,24 @@
-import NextLink from 'next/link'
+import NextLink from 'next/link';
 
 //CSS and UI
-import {Box, Heading, Container, Flex, Text, Link, Image, SimpleGrid, HStack, Tag, TagLabel} from "@chakra-ui/react"
-import {ButtonBlue} from "../../styles/ui-components/Chakra-Button";
-
+import {
+  Box,
+  Heading,
+  Container,
+  Flex,
+  Text,
+  Link,
+  Image,
+  SimpleGrid,
+  HStack,
+  Tag,
+  TagLabel,
+} from '@chakra-ui/react';
+import { ButtonGreen } from '../../styles/ui-components/Chakra-Button';
 
 export default function DeveloperItem() {
   return (
-      <Box
+    <Box
       bg="#ffffff"
       w="100%"
       margin="auto"
@@ -16,24 +27,17 @@ export default function DeveloperItem() {
       boxShadow="0px 0px 2px 4px #e2e8f0"
       p="2%"
       borderRadius="12px"
-      >
-        <NextLink
-        href={"/developers/developer1"}
-        passHref
+    >
+      <NextLink href={'/developers/developer1'} passHref>
+        <Link
+          _hover={{ textDecoration: 'none' }}
+          _focus={{ textDecoration: 'none' }}
         >
-          <Link
-          _hover={{textDecoration: "none"}}
-          _focus={{textDecoration: "none"}}
-          >
-            <Box
-            m="auto"
-            w="fit-content"
-            >
-            <Heading
-              size="md"
-              mb="5px"
-            >Developer #1</Heading>
-              <Image
+          <Box m="auto" w="fit-content">
+            <Heading size="md" mb="5px">
+              Developer #1
+            </Heading>
+            <Image
               m="auto"
               w="6rem"
               h="6rem"
@@ -41,56 +45,33 @@ export default function DeveloperItem() {
               borderRadius="full"
               border="1px solid black"
               src="/DevDAO.png"
-              alt="developer"/>
-              <Text
-              as="i"
-              mb="5px"
-              >Available</Text>
-            </Box>
-          </Link>
-        </NextLink>
+              alt="developer"
+            />
+            <Text as="i" mb="5px">
+              Available
+            </Text>
+          </Box>
+        </Link>
+      </NextLink>
 
-        <HStack
-        m="auto"
-        mt="5%"
-        mb="5%"
-        columns={5}
-        spacing={1}
-        w="fit-content">
-          <Tag
-          size="md"
-          borderRadius="8px"
-          bgColor="#E2E9F0"
-          color="black"
-          ><TagLabel m="auto">Web3</TagLabel></Tag>
-          <Tag
-          size="md"
-          borderRadius="8px"
-          bgColor="#E2E9F0"
-          color="black"
-          ><TagLabel m="auto">Solidity</TagLabel></Tag>
-          <Tag
-          size="md"
-          borderRadius="8px"
-          bgColor="#E2E9F0"
-          color="black"
-          ><TagLabel m="auto">Rust</TagLabel></Tag>
-          <Tag
-          size="md"
-          borderRadius="8px"
-          bgColor="#E2E9F0"
-          color="black"
-          ><TagLabel m="auto">ThreeJS</TagLabel></Tag>
-        </HStack>
+      <HStack m="auto" mt="5%" mb="5%" columns={5} spacing={1} w="fit-content">
+        <Tag size="md" borderRadius="8px" bgColor="#E2E9F0" color="black">
+          <TagLabel m="auto">Web3</TagLabel>
+        </Tag>
+        <Tag size="md" borderRadius="8px" bgColor="#E2E9F0" color="black">
+          <TagLabel m="auto">Solidity</TagLabel>
+        </Tag>
+        <Tag size="md" borderRadius="8px" bgColor="#E2E9F0" color="black">
+          <TagLabel m="auto">Rust</TagLabel>
+        </Tag>
+        <Tag size="md" borderRadius="8px" bgColor="#E2E9F0" color="black">
+          <TagLabel m="auto">ThreeJS</TagLabel>
+        </Tag>
+      </HStack>
 
-        <NextLink
-        href={"/developers/developer1"}
-        passHref
-        >
-          <ButtonBlue>
-            See Profile
-          </ButtonBlue>
-        </NextLink>
-      </Box>
+      <NextLink href={'/developers/developer1'} passHref>
+        <ButtonGreen>See Profile</ButtonGreen>
+      </NextLink>
+    </Box>
   );
 }
--- client/components/navigation/Navbar.tsx
+++ client/components/navigation/Navbar.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import { useEthers } from '@usedapp/core';
-
 import NextLink from 'next/link';
 
-import { chakra, Link, Text, Flex, HStack, Image } from '@chakra-ui/react';
+import {
+  chakra,
+  Link,
+  Flex,
+  HStack,
+  Image,
+  VStack,
+  useDisclosure,
+  IconButton,
+} from '@chakra-ui/react';
+
+import { useEthers } from '@usedapp/core';
+
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
 import { ButtonBlack } from '../../styles/ui-components/Chakra-Button';
 
 function Navbar() {
+  const hamburger = useDisclosure();
+
   const { account } = useEthers();
+
   return (
     <chakra.nav borderBottom="1px solid" borderColor="gray.200">
       <Flex
@@ -39,7 +52,83 @@ function Navbar() {
           </HStack>
         </NextLink>
 
-        <HStack spacing={{ base: 2, sm: 7 }}>
+        <IconButton
+          aria-label=""
+          display={{ sm: 'block', md: 'none' }}
+          zIndex={11}
+          onClick={hamburger.onToggle}
+          icon={
+            hamburger.isOpen ? (
+              <CloseIcon w={7} h={7} />
+            ) : (
+              <HamburgerIcon w={6} h={6} />
+            )
+          }
+        />
+
+        {hamburger.isOpen && (
+          <VStack
+            position="fixed"
+            zIndex={10}
+            top={0}
+            left={0}
+            h="100%"
+            w="100%"
+            display={{ sm: 'flex', md: 'none' }}
+            bg="white"
+            spacing={10}
+            fontSize="3xl"
+            pt={20}
+            onClick={hamburger.onClose}
+          >
+            <NextLink href={'/jobs'} passHref>
+              <Link
+                color="black"
+                p="0.45rem"
+                borderRadius="18px"
+                _hover={{ textDecoration: 'none', bgColor: '#e2e8f0' }}
+                _focus={{ textDecoration: 'none', border: 'none' }}
+              >
+                Jobs
+              </Link>
+            </NextLink>
+            <NextLink href={'/developers'} passHref>
+              <Link
+                color="black"
+                p="0.45rem"
+                borderRadius="18px"
+                _hover={{ textDecoration: 'none', bgColor: '#e2e8f0' }}
+                _focus={{ textDecoration: 'none', border: 'none' }}
+              >
+                Devs
+              </Link>
+            </NextLink>
+            <NextLink href={'/about'} passHref>
+              <Link
+                color="black"
+                p="0.45rem"
+                borderRadius="18px"
+                _hover={{ textDecoration: 'none', bgColor: '#e2e8f0' }}
+                _focus={{ textDecoration: 'none', border: 'none' }}
+              >
+                About
+              </Link>
+            </NextLink>
+            {account ? (
+              <>{account}</>
+            ) : (
+              <NextLink href={'/auth'} passHref>
+                <ButtonBlack as="a">Sign Up</ButtonBlack>
+              </NextLink>
+            )}
+          </VStack>
+        )}
+
+        <HStack
+          display={{ sm: 'none', md: 'flex' }}
+          spacing={{ base: 2, sm: 7 }}
+          mx="auto"
+        >
           <NextLink href={'/jobs'} passHref>
             <Link
               color="black"
@@ -76,10 +165,12 @@ function Navbar() {
         </HStack>
 
         {account ? (
-          <Text>{account}</Text>
+          <>{account}</>
         ) : (
           <NextLink href={'/auth'} passHref>
-            <ButtonBlack as="a">Sign Up</ButtonBlack>
+            <Link display={{ sm: 'none', md: 'flex' }}>
+              <ButtonBlack as="a">Sign Up</ButtonBlack>
+            </Link>
           </NextLink>
         )}
       </Flex>
