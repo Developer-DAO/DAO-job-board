31af75a04aa8842c5f1166e3aaaa41eaf5dc84ae
PBillingsby <philbillingsby@gmail.com>
2021-11-27 11:42:00 -0800

    Add dynamic options for select element for position and title

---
+++ client/constants/optionsForSelect.ts
@@ -0,0 +1,19 @@
+export const jobPositions = [
+  'Co-Founder',
+  'Engineering',
+  'Marketing',
+  'Sales',
+  'Design',
+  'Community',
+  'Art',
+  'Customer Support',
+  'Writing',
+  'Other',
+];
+
+export const employmentTypes = [
+  'Full-Time',
+  'Part-Time',
+  'Contract',
+  'Paid Internship',
+];
