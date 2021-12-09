b2758b444742fd6ed2c765eae3b0d34434247b98
Phil B <45179070+PBillingsby@users.noreply.github.com>
2021-12-09 15:22:21 -0800

    Add i18n to job_summary (#151)

--- packages/client/components/jobs/JobFilter.tsx
+++ packages/client/components/jobs/JobFilter.tsx
@@ -1,6 +1,9 @@
 import { Box, Container, Input } from '@chakra-ui/react';
+import { useTranslation } from 'next-i18next';
 
 function JobFilter() {
+  const { t } = useTranslation('common');
+
   return (
     <Box mt="2.5%">
       <Container my="5px">
@@ -8,7 +11,7 @@ function JobFilter() {
           borderColor="#e2e8f0"
           bgColor="white"
           _hover={{ borderColor: '#97c0e6' }}
-          placeholder="Search a keyword (e.g. Solidity, Front-End, Full-Stack, etc.)"
+          placeholder={t('components.jobs.jobs_filter.placeholder')}
         />
       </Container>
     </Box>
