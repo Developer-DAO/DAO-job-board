68f094214976ef86cf64f074de0b13c835d5f87f
PBillingsby <philbillingsby@gmail.com>
2021-12-09 16:05:55 -0800

    Add email and remove website from users table

--- packages/client/types/user.ts
+++ packages/client/types/user.ts
@@ -3,13 +3,13 @@ import { PrimaryKey, WithTimestamp } from './common';
 export type User = PrimaryKey & {
   avatar_url?: string;
   nonce?: string;
+  email?: string;
   username?: string;
   name?: string;
   description?: string;
   title?: string;
   status?: string;
   location?: string;
-  website?: string;
   disabled_at?: Date;
 } & WithTimestamp;
 
@@ -21,11 +21,11 @@ export type UserKeyword = {
 export type UserForm = Pick<
   User,
   | 'avatar_url'
+  | 'email'
   | 'username'
   | 'name'
   | 'description'
   | 'title'
   | 'status'
   | 'location'
-  | 'website'
 >;
