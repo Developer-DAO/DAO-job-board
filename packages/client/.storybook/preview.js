6172c3d7558963fc7c11a8ffd43b16a52c2f5019
Angel <angel333gomez@gmail.com>
2021-12-17 14:06:08 -0300

    Added Colors to Storybook

--- packages/client/.storybook/preview.js
+++ packages/client/.storybook/preview.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
 
 
 export const decorators = [
   (Story) => (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Story />
     </ChakraProvider>
   ),
