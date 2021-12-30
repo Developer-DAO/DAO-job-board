c177e79443b6a665adde1784820ef78fa8b4749e
Angel <angel333gomez@gmail.com>
2021-12-30 00:37:07 -0300

    Fixing Responsiveness

--- packages/client/pages/jobs/index.tsx
+++ packages/client/pages/jobs/index.tsx
@@ -1,29 +1,40 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import JobFilter from '../../components/jobs/JobFilter';
-import JobCard from '../../components/jobs/JobCard';
+// import JobFilter from '@/components/jobs/JobFilter';
+import JobCard from '@/components/jobs/JobCard';
 
-import { Job } from '../../types/job';
-import { Container, Flex, Heading, Text } from '@chakra-ui/react';
+import { Job } from '@/types/job';
+import { Box, Grid, Text } from '@chakra-ui/react';
 
 export default function Index() {
   return (
-    <Container
-      maxW={{ lg: '70%', md: '90%', sm: '100%' }}
+    <Box
+      maxW="85%"
       m="auto"
-      pt={{ '2xl': '5%', lg: '7%', sm: '20%', base: '17%' }}
-      pl={{ '2xl': '5%', lg: '12%', sm: 'none', base: 'none' }}
+      pt={{ lg: '7%', sm: '20%', base: '17%' }}
+      pr={{ lg: '12%', sm: 'none', base: 'none' }}
+      pl={{ lg: '15%', md: '2%', sm: 'none', base: 'none' }}
       textAlign="center"
     >
       <Text as="i">
         Curated Web3 and blockchain jobs at companies or startups.
       </Text>
-      <Flex maxW="100%" mt="2.5%" w="100%" wrap="wrap">
+      <Grid
+        templateColumns={{
+          '2xl': 'repeat(3, 2fr)',
+          lg: 'repeat(2, 2fr)',
+          md: 'repeat(2, 2fr)',
+          sm: 'repeat(1, 1fr)',
+          base: 'repeat(1, 1fr)',
+        }}
+        gap="1"
+        maxW="100vw"
+      >
         <JobCard {...({} as Job)} />
         <JobCard {...({} as Job)} />
         <JobCard {...({} as Job)} />
-      </Flex>
-    </Container>
+      </Grid>
+    </Box>
   );
 }
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
