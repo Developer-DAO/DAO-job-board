27958a4662c439543a7da78a4c96f06c534d39a5
Angel <angel333gomez@gmail.com>
2022-01-08 00:00:02 -0300

    Developing JobModal

--- packages/client/components/modals/jobs/JobPost.tsx
+++ packages/client/components/modals/jobs/JobPost.tsx
@@ -1,4 +1,14 @@
-import { Heading, Text, HStack, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Image,
+  Text,
+  HStack,
+  VStack,
+  Stack,
+  Button,
+  Grid,
+} from '@chakra-ui/react';
 
 type CompanyInfo = {
   name: string;
@@ -36,19 +46,67 @@ export default function JobPostModal({
       bg="neutral.100"
       zIndex="modal"
     >
-      <JobModalHeader position={position} />
+      <JobModalHeader
+        position={position}
+        compensation={compensation}
+        companyInfo={companyInfo}
+      />
+      <JobModalContent description={description} />
+      <JobModalFooter closeJobModal={closeJobModal} />
     </Stack>
   );
 }
 
-const JobModalHeader = ({ position }: any) => {
+const JobModalHeader = ({ position, compensation, companyInfo }: any) => {
   return (
-    <HStack>
-      <Heading fontSize="header1">{position}</Heading>
+    <HStack textAlign="start">
+      <Image
+        h="4rem"
+        w="4rem"
+        borderRadius="10px"
+        m="30px"
+        src={companyInfo.logo_url}
+      />
+
+      <VStack alignItems="left">
+        <Heading fontSize="header5">
+          {position} - {companyInfo.name}
+        </Heading>
+        <Text fontSize="caption" color="neutral.400" fontWeight="semibold">
+          {compensation} / month
+        </Text>
+      </VStack>
     </HStack>
   );
 };
 
 const JobModalContent = ({ description }: any) => {
-  return <Stack>{description}</Stack>;
+  return (
+    <Grid>
+      <Box textAlign="justify">
+        <Text fontSize="body3">{description}</Text>
+      </Box>
+    </Grid>
+  );
+};
+
+const JobModalFooter = ({ closeJobModal }: any) => {
+  return (
+    <Stack>
+      <Button
+        color="white"
+        bgColor="neutral.900"
+        _hover={{ bgColor: 'neutral.500' }}
+      >
+        Apply for Job
+      </Button>
+      <Button
+        color="white"
+        bgColor="danger.500"
+        _hover={{ color: 'black', bgColor: 'danger.300' }}
+      >
+        Ignore
+      </Button>
+    </Stack>
+  );
 };
