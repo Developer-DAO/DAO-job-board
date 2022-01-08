9330d0b991ab73214f51e5594ed3ac1e9f78ffb0
Angel Gomez <53134194+angeljgomezc@users.noreply.github.com>
2022-01-08 18:52:45 -0300

    Revert "Modify Filter - Turning New Designs Into Code (#175)"
    This reverts commit 9a627d2cb3dbcea1a99d835a5a470e5f182d3968.

--- packages/client/pages/jobs/index.tsx
+++ packages/client/pages/jobs/index.tsx
@@ -1,27 +1,17 @@
-import JobFilter from '@/components/jobs/JobFilter';
-import JobList from '@/components/jobs/JobList';
-import { Box, Grid, GridItem } from '@chakra-ui/react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import JobList from '@/components/jobs/JobList';
+
+import { Box } from '@chakra-ui/react';
 
 export default function Index() {
   return (
-    <>
-      <Grid templateColumns="250px repeat(4, 1fr)" gap="1">
-        <GridItem colStart={2} colEnd={5} overflow="hidden">
-          <Box
-            pt={{ '2xl': '5%', lg: '7%', sm: '17%', base: '15%' }}
-            pl={{ '2xl': '5%', lg: '12%', sm: 'none', base: 'none' }}
-          >
-            <JobList />
-          </Box>
-        </GridItem>
-        <GridItem colStart={5}>
-          <Box pt={{ '2xl': '5%', lg: '7%', sm: '17%', base: '15%' }}>
-            <JobFilter />
-          </Box>
-        </GridItem>
-      </Grid>
-    </>
+    <Box
+      pt={{ '2xl': '5%', lg: '7%', sm: '17%', base: '15%' }}
+      pl={{ '2xl': '5%', lg: '12%', sm: 'none', base: 'none' }}
+    >
+      <JobList />
+    </Box>
   );
 }
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
